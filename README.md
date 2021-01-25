6a416bb98d45a5e738587fd291e804e6552a2893
unknown <ky91zzang@naver.com>
2021-01-25 12:19:23 +0900

    Auth process comp

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ mongo key값 숨김
 bcrypt  
 
   ## 2021-01-25
-  로그인 기능
-  웹 토큰 생성 라이브러리 추가
+  ### 로그인 기능
+  #### 웹 토큰 생성 라이브러리 추가
   npm install jsonwebtoken --save
-  쿠키파서 라이브러리 추가
+  #### 쿠키파서 라이브러리 추가
   npm install cookie-parser --save
+  user_id ==> 쿠키에 넣으려면 toHexString
+
+  ### Auth 기능
+  페이지가 이동 할때 마다 쿠키에 들어있는 토큰과 DB 들어있는 토큰이 같은지를 비교하여 활동 권한을 부여
+  middleware 폴더에 auth를 처리해서 인증
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 5000
 const {User}  = require('./models/User')
 const bodyParser  = require('body-parser');
 const cookieParser = require('cookie-parser');
+const {auth} = require('./middleware/auth');
 
 const config = require('./config/key');
 
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!히티하티호')
 })
 
-app.post("/register",(req,res)=>{
+app.post("/api/users/register",(req,res)=>{
     //회원가입할때 필요한 정보를 client에서 가져오면
     //그것들을 DB에 넣어준다.
     const user = new User(req.body)
@@ -42,10 +43,10 @@ app.post("/register",(req,res)=>{
 
 });
 
-app.post('/login',(req,res)=>{
+app.post('/api/users/login',(req,res)=>{
   //요청된 이메일을 데이터 베이스에서 있는지 찾는다.
   User.findOne({email:req.body.email},(err,user)=>{ //몽고디비 함수
-    if(!user){
+    if(!user){ //반환된 유저정보가 없는 경우
       return res.json({
         loginSuccess : false,
         message : "제공된 이메일에 해당하는 유저가 없습니다."
@@ -62,15 +63,41 @@ app.post('/login',(req,res)=>{
         if(err) return res.status(400).send(err);
         // 토큰을 저장한다. 어디? 쿠키, 로컬 스토리지, 등등에 저장가능
         // 이번에는 쿠키
-         res.cookie("x_auth",user.token)
-         .status(200)
-         .json({loginSuccess:true,userId : user._id})
-        //이렇게 하면 웹 쿠키 부분에 x_auth 로 저장됨
+         res.cookie("x_auth",user.token) //이렇게 하면 웹 쿠키 부분에 x_auth 로 저장됨
+         .status(200) //통신이 정상적일때
+         .json({loginSuccess:true,userId : user._id}) //loginSuccess 부분을 true로 하고 user.id 반환 (HexString)
   
       })
     })
   })
 })
+
+//auth 권한 처리
+app.get('/api/user/auth',auth,(req,res)=>{
+  //auth 는 미들웨어
+  // 여기까지 통과되면 req.token, req.user 정보 확인가능
+
+    //클라이언트에 정보 전달 가능
+    //role 0 ->일반 유저
+    //role 1 -> 관리자
+    res.status(200).json({
+      _id : req.user._id,
+      isAdmin : req.user.role === 0 ? false : true,
+      email : req.user.email,
+      name : req.user.name,
+      lastname : req.user.lastname,
+      role : req.user.role,
+      image : req.user.image
+    })
+})
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
